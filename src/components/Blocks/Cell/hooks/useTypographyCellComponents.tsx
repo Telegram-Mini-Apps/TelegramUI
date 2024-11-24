ad4ae23931f854e8941a96952aeca4af7c42334f
Bulat Shelkhonov <shelbulat@mail.ru>
2024-11-24 13:09:12 +0300

    fix dom rebuild in cell

--- src/components/Blocks/Cell/hooks/useTypographyCellComponents.tsx
+++ src/components/Blocks/Cell/hooks/useTypographyCellComponents.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useCallback } from 'react';
+
 import { usePlatform } from 'hooks/usePlatform';
 
 import { Caption } from 'components/Typography/Caption/Caption';
@@ -11,21 +13,21 @@ export const useTypographyCellComponents = () => {
   const platform = usePlatform();
   const isIOS = platform === 'ios';
 
-  const Title = (props: TypographyProps) => {
+  const Title = useCallback((props: TypographyProps) => {
     if (isIOS) {
       return <Text {...props} />;
     }
 
     return <Subheadline level="1" {...props} />;
-  };
+  }, [isIOS]);
 
-  const Description = (props: TypographyProps) => {
+  const Description = useCallback((props: TypographyProps) => {
     if (isIOS) {
       return <Caption {...props} />;
     }
 
     return <Subheadline level="2" {...props} />;
-  };
+  }, [isIOS]);
 
   return {
     Title,
