9e6ab27187997e04705d37df055b06f7102ff66a
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-06-02 07:18:42 +0300

    refactor: define component as button and fix styles

--- src/components/Blocks/Cell/components/NavigationCell/NavigationCell.tsx
+++ src/components/Blocks/Cell/components/NavigationCell/NavigationCell.tsx
@@ -1,4 +1,7 @@
 import type { ReactNode } from 'react';
+import styles from './NavigationCell.module.css';
+
+import { classNames } from 'helpers/classNames';
 
 import { IconContainer } from 'components/Blocks/IconContainer/IconContainer';
 import type { CellProps } from '../../Cell';
@@ -16,12 +19,16 @@ export function NavigationCell({
   after,
   icon,
   color,
+  className,
+  Component,
   ...props
 }: NavigationCellProps) {
   return (
     <Cell
+      Component={Component || 'button'}
       before={icon && <IconContainer color={color}>{icon}</IconContainer>}
       after={<Navigation>{after}</Navigation>}
+      className={classNames(styles.wrapper, className)}
       {...props}
     />
   );
